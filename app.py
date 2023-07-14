3f0c2be446c8ef063e0d57e9f357e54010389123
Luis Yamada <luishm.yamada@gmail.com>
2023-07-14 00:51:29 -0300

    wip - v0.0.2

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 import requests
+import os
 from selenium import webdriver
+
+
+html_dir_path = './staging_html_objects/'
 
 
 def search_for_new_chapter(link):
     browser = webdriver.Chrome()
     browser.get(link)
-    with open(f'./staging_html_objects/{link.split("/")[-2]}.html', "w", encoding='utf-8') as f:
+    with open(f'{html_dir_path}{link.split("/")[-2]}.html', "w", encoding='utf-8') as f:
         f.writelines(browser.page_source)
     browser.close
 
 
 def main():
+    if os.path.exists(html_dir_path) is False:
+        os.mkdir(html_dir_path)
+
     search_for_new_chapter('https://mangalivre.net/manga/black-clover/1751')
     search_for_new_chapter('https://mangalivre.net/manga/doku-doku-mori-mori/17358')
     search_for_new_chapter('https://mangalivre.net/manga/my-dragon-system/17292')
